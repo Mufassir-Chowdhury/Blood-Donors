3f017c227ba412cfc412a13274be3972642052f9
Mufassir-Chowdhury <mac22214u@gmail.com>
2021-04-04 02:23:28 +0600

    minor UI fixes

--- src/Pages/Contact.vue
+++ src/Pages/Contact.vue
@@ -1,20 +1,107 @@
 <template>
-<div class="h-screen">
+<!-- <div class="h-screen back">
     <Header :Page="PageName"/>
-    <div class="h-full min-w-screen flex items-center justify-items-center justify-center">
+    <div class="md:grid md:grid-cols-2 items-center justify-items-center justify-center h-full">
+        <div class="md:col-span-1 w-full items-center justify-items-center justify-center">
+            <button class="px-4 py-2 flex items-center text-base mb-4 rounded-full text-indigo-500 border border-indigo-500 hover:text-white hover:bg-indigo-500 undefined ">
+                <svg width="20" fill="currentColor" height="20" class="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z">
+                    </path>
+                </svg>
+                Facebook
+            </button>
+            <button class="px-4 py-2 flex items-center text-base rounded-full text-indigo-500 border border-indigo-500 hover:text-white hover:bg-indigo-500 undefined ">
+                <svg width="20" fill="currentColor" height="20" class="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z">
+                    </path>
+                </svg>
+                Facebook
+            </button>
+        </div>
+        <div class="md:col-span-1 h-full min-w-screen flex items-center justify-items-center justify-center">
+            <form class="flex w-full max-w-sm space-x-3" @submit.prevent="send">
+                <div class="w-full max-w-2xl px-5 py-10 m-auto  bg-white rounded-xl shadow dark:bg-gray-800">
+                    <div class="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white">
+                        Contact us !
+                    </div>
+                    <div class="max-w-xl m-auto">
+                        <div class="my-4">
+                            <div class=" relative ">
+                                <input v-model="form.Name" type="name" id="contact-form-name" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name"/>
+                            </div>
+                        </div>
+                        <div class="my-4">
+                            <div class=" relative ">
+                                <input v-model="form.Mobile" type="tel" id="contact-form-mobile" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Mobile"/>
+                            </div>
+                        </div>
+                        <div class="my-4">
+                            <label class="text-gray-700" for="name">
+                                <textarea v-model="form.Comment" class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" id="comment" placeholder="Enter your comment" name="comment" rows="5" cols="40">
+                                </textarea>
+                            </label>
+                        </div>
+                        <div class="text-right my-4">
+                            <button type="submit" class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                Send
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div> -->
+<Header :Page="PageName"/>
+<div class="h-screen lg:grid lg:grid-cols-12">
+    <div class="py-10 lg:col-span-5 lg:bg-gray-300 justify-items-center items-center justify-center lg:h-full flex flex-col">
+        <div class="text-4xl font-extrabold mb-4">Get in touch</div>
+        <div>"গড়বো রক্তের বন্ধন
+        দূর হবে সব ক্রন্দন"</div>
+        <div class="flex my-4">
+            <svg class="w-6 h-6 mx-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            Sylhet - 3100
+        </div>
+        <div class="flex my-4">
+            <svg class="w-6 h-6 mx-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+            </svg>
+            +88 01710-706044
+        </div>
+        <div class="flex my-1 align-text-bottom">
+            <div class="border mx-2 border-gray-700 p-1 rounded-md">
+                <svg fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z">
+                    </path>
+                </svg>
+            </div>
+            <div class="mt-1">
+                হৃদম ব্লাড ডোনেশন ক্লাব, সিলেট
+            </div>
+        </div>        
+    </div>
+    <div class="lg:hidden grid grid-cols-12 gap-2 mt-6">
+        <div class="mt-2 ml-2 border-t border-gray-400 col-span-5"></div>
+        <div class="col-span-2 text-center">OR</div>
+        <div class="mt-2 mr-2 border-t border-gray-400 col-span-5"></div>
+    </div>
+    <div class="lg:col-span-7 lg:h-full min-w-screen flex items-center justify-items-center justify-center">
         <form class="flex w-full max-w-sm space-x-3" @submit.prevent="send">
-            <div class="w-full max-w-2xl px-5 py-10 m-auto  bg-white rounded-xl shadow dark:bg-gray-800">
+            <div class="w-full max-w-2xl px-5 py-10 m-auto  bg-white lg:rounded-xl shadow dark:bg-gray-800">
                 <div class="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white">
                     Contact us !
                 </div>
                 <div class="max-w-xl m-auto">
                     <div class="my-4">
-                        <div class=" relative ">
+                        <div>
                             <input v-model="form.Name" type="name" id="contact-form-name" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name"/>
                         </div>
                     </div>
                     <div class="my-4">
-                        <div class=" relative ">
+                        <div>
                             <input v-model="form.Mobile" type="tel" id="contact-form-mobile" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Mobile"/>
                         </div>
                     </div>
@@ -66,5 +153,8 @@ export default {
 </script>
 
 <style>
-
+.back {
+    background-color: #ffffff;
+    background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%236d6d6f' fill-opacity='0.34' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
+}
 </style>
--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,29 +1,29 @@
 <template>
-
-<div class="bg-gradient-to-br from-pink-700 to-red-300 relative overflow-hidden min-h-screen">
+<!-- bg-gradient-to-br from-pink-700 to-red-300  -->
+<div class="relative overflow-hidden min-h-screen back">
     <header class="absolute top-0 left-0 right-0 z-20">
         <nav class="container mx-auto px-6 md:px-12 py-4">
             <div class="md:flex justify-center items-center">
                 <div class="hidden md:flex items-center">
-                    <router-link to="/Blood-Donors/list" class="border-2 border-white rounded-lg text-lg uppercase px-3 mx-3 pt-1 text-white cursor-pointer hover:text-gray-300">
+                    <router-link to="/Blood-Donors/list" class="rounded-lg text-lg uppercase px-3 mx-3 pt-1 text-black cursor-pointer hover:text-gray-300">
                         Donor List
                     </router-link>
-                    <router-link to="/Blood-Donors/groups" class="border-2 border-white rounded-lg text-lg uppercase px-3 mx-3 pt-1 text-white cursor-pointer hover:text-gray-300">
+                    <router-link to="/Blood-Donors/groups" class="rounded-lg text-lg uppercase px-3 mx-3 pt-1 text-black cursor-pointer hover:text-gray-300">
                         Blood Groups
                     </router-link>
-                    <router-link to="/Blood-Donors/contact" class="border-2 border-white rounded-lg text-lg uppercase px-3 mx-3 pt-1 text-white cursor-pointer hover:text-gray-300">
+                    <router-link to="/Blood-Donors/contact" class="rounded-lg text-lg uppercase px-3 mx-3 pt-1 text-black cursor-pointer hover:text-gray-300">
                         Contact
                     </router-link>
                 </div>
             </div>
         </nav>
     </header>
-    <div class="container border-4 border-white rounded-lg mx-auto w-4/5 relative z-10 flex items-center py-16 my-24 md:my-32">
+    <div class="container rounded-lg mx-auto w-4/5 relative z-10 flex items-center py-16 my-24 md:my-32">
         <div class="w-full flex flex-col items-center justify-between relative z-10">
-            <p class="flex flex-col items-center font-extrabold text-4xl text-center md:text-6xl text-white">
-                Rhythm Blood Donation Club, Sylhet
+            <p class="items-center font-extrabold text-4xl text-center md:text-6xl text-black">
+                Rhythm <span class="text-pink-500"> Blood Donation </span> Club, <span class="text-indigo-600"> Sylhet City </span>
             </p>
-            <p class="px-4 flex flex-col max-w-lg text-center items-center font-extrabold text-sm md:text-lg mt-6 text-white">
+            <p class="px-4 max-w-lg text-center items-center font-thin text-sm md:text-lg mt-6 text-gray-500 leading-loose">
                 A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation. Donation may be of whole blood, or of specific components directly.
             </p>
             <router-link to="/Blood-Donors/join" class="block bg-pink-600 hover:bg-gray-600 py-3 rounded-md px-4 text-lg text-white font-bold uppercase mt-10">
@@ -34,17 +34,17 @@
         <div class="h-screen md:hidden lg:h-auto min-w-full items-center flex justify-center text-center py-12">
         <ul>
             <router-link to="/Blood-Donors/list">
-                <li class="cursor-pointer rounded-lg shadow-lg py-4 text-white text-xl font-bold h-16 bg-pink-600 w-64 my-10">
+                <li class="cursor-pointer rounded-lg shadow-lg py-4 text-white text-xl font-bold h-16 bg-pink-600 w-56 my-10">
                     Search from list
                 </li>
             </router-link>
             <router-link to="/Blood-Donors/groups">
-                <li class="cursor-pointer rounded-lg shadow-lg py-4 text-white text-xl font-bold h-16 bg-pink-600 w-64 my-10">
+                <li class="cursor-pointer rounded-lg shadow-lg py-4 text-white text-xl font-bold h-16 bg-pink-600 w-56 my-10">
                     Search by groups
                 </li>
             </router-link>
             <router-link to="/Blood-Donors/contact">
-                <li class="cursor-pointer rounded-lg shadow-lg py-4 text-white text-xl font-bold h-16 bg-pink-600 w-64 my-10">
+                <li class="cursor-pointer rounded-lg shadow-lg py-4 text-white text-xl font-bold h-16 bg-pink-600 w-56 my-10">
                     Contact us!
                 </li>
             </router-link>
@@ -62,5 +62,8 @@ export default {
 </script>
 
 <style>
-
+.back {
+    background-color: #ffffff;
+    background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%236d6d6f' fill-opacity='0.34' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
+}
 </style>
